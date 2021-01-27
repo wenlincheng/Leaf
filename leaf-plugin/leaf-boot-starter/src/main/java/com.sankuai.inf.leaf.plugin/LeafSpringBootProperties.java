5c3bc00add3d1646de650df1e6e0b19c23e49d39
wenlincheng <wenlcmax@gmail.com>
2021-01-27 16:14:57 +0800

    修改配置文件写在application.yml中

--- leaf-plugin/leaf-boot-starter/src/main/java/com.sankuai.inf.leaf.plugin/LeafSpringBootProperties.java
+++ leaf-plugin/leaf-boot-starter/src/main/java/com.sankuai.inf.leaf.plugin/LeafSpringBootProperties.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties(prefix = "leaf",ignoreUnknownFields = true)
-@PropertySource("classpath:leaf.properties")
+// @PropertySource("classpath:leaf.properties")
 public class LeafSpringBootProperties {
     private String name;
     private Segment segment;
